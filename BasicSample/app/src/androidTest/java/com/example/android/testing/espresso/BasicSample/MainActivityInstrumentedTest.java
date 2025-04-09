91c1a7cb6a5a4c6ce06d50a45adf1fe3e7708894
Nischal-Shrestha-1 <nis.shrestha.1@gmail.com>
2025-04-09 16:38:34 -0400

    added

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.java
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.java
@@ -1,3 +1,27 @@
+package com.example.android.testing.espresso.BasicSample;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.MediumTest;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import com.example.android.testing.espresso.BasicSample.MainActivity;
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
 public class MainActivityInstrumentedTest {
-    
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class); 
 }
