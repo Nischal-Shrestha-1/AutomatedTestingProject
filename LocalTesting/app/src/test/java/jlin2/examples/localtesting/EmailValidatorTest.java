4e71484620d6dd2fd5b1625d8f34583521a49d09
DRP07 <84800150+DRP07@users.noreply.github.com>
2025-04-09 16:30:12 -0400

    Update EmailValidatorTest.java

--- LocalTesting/app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.java
+++ LocalTesting/app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.java
@@ -4,4 +4,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class EmailValidatorTest {
+       @Test
+    public void testValidEmail() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.com"));
+    }
+
+    @Test
+    public void testValidSubdomainEmail() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.co.ca"));
+    }
+
+    @Test
+    public void testInvalidEmail_NoDomain() {
+        assertFalse(EmailValidator.isValidEmail("123@abc"));
+    }
+
+    @Test
+    public void testInvalidEmail_DoubleDots() {
+        assertFalse(EmailValidator.isValidEmail("123@abc..com"));
+    }
+    
 }
